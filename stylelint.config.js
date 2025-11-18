a6abf10ecf473ff252e34921051070149cd77d37
GenFront <genti7042@gmail.com>
2025-11-18 13:04:00 +0300

    Продолжаю создавать библиотеку

---
+++ stylelint.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-scss'
+  ],
+  rules: {
+    'color-function-notation': 'modern',
+    'alpha-value-notation': 'number'
+  }
+};
